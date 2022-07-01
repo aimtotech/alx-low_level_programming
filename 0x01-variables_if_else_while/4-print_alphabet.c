1fc63b03b37db6bd1b0ef951506e5b02bf600f6d
Amina <isamusamina@gmail.com>
2022-07-01 16:52:54 -0700

    var

--- 0x01-variables_if_else_while/4-print_alphabet.c
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
-
 #include <time.h>
 #include <stdio.h>
+
 /**
  *  main - Entry point
  *
@@ -12,7 +12,6 @@ int main(void)
 	char alpha;
 
 	for (alpha = 'a'; alpha <= 'z' ; alpha++)
-	if (alpha != 'e' && alpha != 'q')
 	putchar(alpha);
 	putchar('\n');
 	return (0);
