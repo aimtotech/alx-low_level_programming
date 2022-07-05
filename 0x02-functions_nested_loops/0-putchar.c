247088fd455cd6ec85240b5553de2ae1fb509354
Amina <isamusamina@gmail.com>
2022-07-05 09:43:59 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints _putchar
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char task[] = "_putchar";
+
+	printf("%s\n", task);
+
+	return (0);
+}
