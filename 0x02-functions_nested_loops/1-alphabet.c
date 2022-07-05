ca735bba7fa30982af70551bfe07473e02cffbf0
Amina <isamusamina@gmail.com>
2022-07-05 13:52:19 -0700

    1fnl

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,13 @@
 #include "main.h"
 
 /**
- * main - print alphabets in lowercase
- *
- * Return: 0 if successful
+ *print_alphabet - print alphabets in lowercase
  */
 void print_alphabet(void)
 {
 	char letters;
 
-	for (letters = 'a'; letters >= 'z'; letters++)
+	for (letters = 'a'; letters <= 'z'; letters++)
 		_putchar(letters);
 
 	_putchar('\n');
