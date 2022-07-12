95547d0dc971b334cee63284b5f2402c2475b992
Amina <isamusamina@gmail.com>
2022-07-12 10:43:57 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen- prints the length of a string
+ * @s: string variable
+ * Return: The length of a string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
